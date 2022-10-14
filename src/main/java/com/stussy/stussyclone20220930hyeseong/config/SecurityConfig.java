603a7cc2149fdd9de1e762f3deb1069c004edaac
leehyeseong0821 <lhs5567@naver.com>
2022-10-14 17:59:45 +0900

    로그인

--- src/main/java/com/stussy/stussyclone20220930hyeseong/config/SecurityConfig.java
+++ src/main/java/com/stussy/stussyclone20220930hyeseong/config/SecurityConfig.java
@@ -1,14 +1,26 @@
 package com.stussy.stussyclone20220930hyeseong.config;
 
+import com.stussy.stussyclone20220930hyeseong.security.AuthFailureHandler;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configurable
 
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+
+        return new BCryptPasswordEncoder();
+    }
+
+
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -17,12 +29,16 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
         http.authorizeHttpRequests()
                 .antMatchers("/account/mypage","/index")
                 .authenticated()
+                .antMatchers("/admin/**")
+                .hasRole("ADMIN")
                 .anyRequest()
                 .permitAll()
                 .and()
                 .formLogin()
+                .usernameParameter("email")
                 .loginPage("/account/login")   //login page
                 .loginProcessingUrl("/account/login") //login service post요청
+                .failureHandler(new AuthFailureHandler())
                 .defaultSuccessUrl("/index");
     }
 
--- src/main/java/com/stussy/stussyclone20220930hyeseong/service/PrincipalDetailsService.java
+++ src/main/java/com/stussy/stussyclone20220930hyeseong/service/PrincipalDetailsService.java
@@ -1,18 +1,43 @@
 package com.stussy.stussyclone20220930hyeseong.service;
 
+import com.stussy.stussyclone20220930hyeseong.domain.User;
+import com.stussy.stussyclone20220930hyeseong.exception.CustomInternalServerErrorException;
+import com.stussy.stussyclone20220930hyeseong.repository.AccountRepository;
+import com.stussy.stussyclone20220930hyeseong.security.PrincipalDetails;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class PrincipalDetailsService implements UserDetailsService{
 
-    @Override
-    public UserDetails loadUserByUsername(String username)  throws UsernameNotFoundException {
-        log.info("loadUserByUsername: {}", username);
+    private final AccountRepository accountRepository;
 
-        return null;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email)  throws UsernameNotFoundException {
+
+        User user = null;
+
+
+
+        try{
+            user = accountRepository.findUserByEmail(email);
+
+        }catch(Exception e){
+            throw new CustomInternalServerErrorException("회원 정보 조회 오류");
+        }
+        if(user == null){
+            throw new UsernameNotFoundException("잘 못된 사용자 정보");
+        }
+
+        return new PrincipalDetails(user);
     }
 
 }
