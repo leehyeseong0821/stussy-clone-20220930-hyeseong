289e4e3cd88e2e6ca6bb26ddaeeb7644cb93be07
leehyeseong0821 <lhs5567@naver.com>
2022-10-19 17:51:41 +0900

    2022-10-19

--- src/main/java/com/stussy/stussyclone20220930hyeseong/aop/ValidationAop.java
+++ src/main/java/com/stussy/stussyclone20220930hyeseong/aop/ValidationAop.java
@@ -19,11 +19,13 @@ import java.util.Map;
 @Component
 public class ValidationAop {
 
-    @Pointcut("execution(* com.stussy.stussyclone20220930hyeseong.api..*Api.*(..))")
-    private void executionPointCut() {}
+//    @Pointcut("execution(* com.stussy.stussyclone20220930hyeseong.api..*Api.*(..))")
+//    private void executionPointCut() {}
+    @Pointcut("@annotation(com.stussy.stussyclone20220930hyeseong.aop.annotation.ValidAspect)")
+    private void annotationPointcut(){
 
-
-        @Around("executionPointCut()")
+    }
+    @Around("annotationPointcut()")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
 
         Object[] args= joinPoint.getArgs();
